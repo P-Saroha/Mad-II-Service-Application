aff66ba8c96f33f88e199288853f6502983994d8
P-Saroha <parveensaroha2002@gmail.com>
2025-06-08 00:56:46 +0530

    fixed_mail_service

--- Backend/celery/mail_service.py
+++ Backend/celery/mail_service.py
@@ -2,23 +2,22 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
 SMTP_SERVER = "localhost"
 SMTP_PORT = 1025
 SENDER_EMAIL = 'Parveen@gmail.com'
-SENDER_PASSWORD = ''
+SENDER_PASSWORD = ''  # Not used here but typically needed for real SMTP services
 
 def send_email(to, subject, content):
-
     msg = MIMEMultipart()
     msg['To'] = to
     msg['Subject'] = subject
     msg['From'] = SENDER_EMAIL
 
-    msg.attach(MIMEText(content,'html'))
+    msg.attach(MIMEText(content, 'html'))
 
     with smtplib.SMTP(host=SMTP_SERVER, port=SMTP_PORT) as client:
         client.send_message(msg)
-        client.quit()
 
-send_email('parveen@example', 'Test Email', '<h1> Welcome to Household Services Application  </h1>')
+# Run only for local testing
+if __name__ == "__main__":
+    send_email('parveen@example', 'Test Email', '<h1> Welcome to Household Services Application </h1>')
